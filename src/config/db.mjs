adaa54222c34bdbd8495d0bc4c93dec9bf434169
ABDOULLAH EL-HADRAMY <abdoullahhadramy@gmail.com>
2025-02-19 23:31:03 -0500

    add console.log to verifie if the connection happened succefully

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const mongoUri = process.env.MONGO_URI;
+    console.log(`Connecting to MongoDB with URI: ${mongoUri}`); // Log the URI to verify
+
+    const conn = await mongoose.connect(mongoUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
